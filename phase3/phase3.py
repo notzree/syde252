463e612cdab070c587cadf865b94438b656306e1
notzree <notzree@gmail.com>
2024-11-25 21:00:32 -0500

    init phase3.py commit

---
+++ phase3/phase3.py
@@ -0,0 +1,11 @@
+"""
+Python 3.12.6
+
+To install dependencies run either:
+pip install librosa matplotlib numpy soundfile scipy
+or
+pip install -r requirements.txt (requires python 3.12.6 for compatibility)
+
+To run:
+python phase3.py
+"""
